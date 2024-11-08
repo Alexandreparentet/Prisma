11e807603ecf0b92e55bd5ec19b296bc977e67f3
unknown <ALEXANDRE6071@digitalcollegesul.local>
2024-11-08 17:12:08 -0300

    Create Data

--- server.js
+++ server.js
@@ -1,1 +1,13 @@
-import { PrismaClient } from "@prisma/client"; // Insert Into, Drop, Update, Select
+import { PrismaClient } from '@prisma/client'; // Insert Into, Drop, Update, Select
+
+
+const prisma = new PrismaClient();
+
+const result = await prisma.user.create({
+    data: {
+        name: 'levy',
+        email: 'ale@homail.com'
+    }
+})
+
+console.log(result);
